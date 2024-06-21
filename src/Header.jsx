a2908f4019f7885cb114e3478fb0cc281584e8c4
kg2020 <kg2020@hw.ac.uk>
2024-06-21 15:21:25 +0200

    progress on responsiveness

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,9 +4,7 @@ function Header() {
     return (
         <div className={Styles.header}>
             <div className={Styles.heading}>
-                <h1>Specialist</h1>
-                <h1>Entertainment</h1>
-                <h1>Agency.</h1>
+                <h1>Specialist Entertainment Agency.</h1>
             </div>
             <p className={Styles.headingP}>
                 From strategy to execution to content, we deliver answers for
